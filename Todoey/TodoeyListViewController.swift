0c875bcfd753ea0e23f692aef45af29bcee62f33
Darshan Sk <darshansk@darshans-macbook-pro.local>
2018-10-20 20:43:00 +0530

    Hard Coded Entires

---
+++ Todoey/TodoeyListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Darshan Sk on 10/20/18.
+//  Copyright © 2018 Darshan Sk. All rights reserved.
+//
+
+import UIKit
+
+class TodoeyListViewController: UITableViewController {
+    let itemArray = ["First", "Second", "Third"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoeyItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
